05fda9c29fe0c011336d3dc850b1dc07801e61c7
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-05-04 21:06:34 +0300

    Add files via upload

--- src/main/java/service/CodeGenerator.java
+++ src/main/java/service/CodeGenerator.java
@@ -1,1 +1,24 @@
+package service;
 
+import org.apache.log4j.Logger;
+
+import java.util.Random;
+
+public class CodeGenerator {
+    private static final Random random = new Random();
+    private static final Logger logger = Logger.getLogger(CodeGenerator.class);
+
+    public static int generateCode() {
+        int limit = 9999;
+        int randomNumber = random.nextInt(limit);
+        logger.info("Generating Code");
+        return randomNumber;
+    }
+
+    public static String generateId(String str) {
+        str = str.replace(" ", "");
+        logger.info("Generating Id");
+        return str.substring(0, 4) + generateCode();
+    }
+
+}
